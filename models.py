c055aee428606cdf5e954ffe0482ade97e15b118
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-04-07 22:11:11 +0800

    Updated models and server to connect with the database

---
+++ models.py
@@ -0,0 +1,22 @@
+import os
+import peewee as pw
+import datetime
+from playhouse.postgres_ext import PostgresqlExtDatabase
+
+db = PostgresqlExtDatabase(os.getenv('DATABASE'))
+
+class BaseModel(pw.Model):
+    created_at = pw.DateTimeField(default=datetime.datetime.now)
+    updated_at = pw.DateTimeField(default=datetime.datetime.now)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.datetime.now()
+        return super(BaseModel, self).save(*args, **kwargs)
+
+    class Meta:
+        database = db
+        legacy_table_names = False
+
+class Transaction(BaseModel):
+    text = pw.CharField()
+    amount = pw.CharField()
